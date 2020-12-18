7126ba232febecaff2259c17164377afdaec6890
Jackson Cai <jcai8649@gmail.com>
2020-12-17 18:49:11 -0800

    Fix UI for login

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -5,19 +5,18 @@ import { Button, Input } from '@material-ui/core';
 
 export default function Login() {
 
-    const [login, setLogin] = useState({
-        isLogin: false
-    })
+    const [login, setLogin] = useState(false)
 
     const [loginData, setLoginData] = useState({
-        email: null,
-        password: null
+        email: '',
+        password: ''
     })
 
 
     const submitHandler = (e) => {
         e.preventDefault();
         postUserHandler();
+        resetLoginDataHandler();
     }
 
     const postUserHandler = () => {
@@ -25,19 +24,23 @@ export default function Login() {
         axios.post('https://cai-task-manager.herokuapp.com/users/login', loginData)
         .then(response => {
             setLogin({isLogin: true})
-            console.log(`Is login! ${loginData.email}`)
+            console.log(`Welcome, ${response.data.user.name}`)
         });
     }
 
+    const resetLoginDataHandler = () => {
+        setLoginData({email: '', password: ''})
+    }
+    
     return (
         <div className={styles.Auth}>
             <h1>Sign In</h1>
             <form onSubmit={submitHandler}>
                 <div>
-                    <Input type='text' name='email'  onChange={ e => {setLoginData({...loginData, email: e.target.value})}}placeholder='email'></Input>
+                    <Input type='text' name='email'  value={loginData.email} onChange={ e => {setLoginData({...loginData, email: e.target.value})}}placeholder='email'></Input>
                 </div>
                 <div>
-                    <Input type='password' onChange={e => setLoginData({...loginData, password: e.target.value})}name='pass' placeholder='password'></Input>
+                    <Input type='password' value={loginData.password} onChange={e => setLoginData({...loginData, password: e.target.value})}name='pass' placeholder='password'></Input>
                 </div>
                 <div>
                     <Button type="submit">Login</Button>

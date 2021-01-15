65e44febfa4a33afb1bbbb9efd97e1a80729baef
Jackson Cai <jcai8649@gmail.com>
2021-01-14 17:49:11 -0800

    Create delete button

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import styles from './Task.module.css'
+import {Button} from '@material-ui/core'
 
 function Task({description}) {
     return (
-        <li>{description}</li>
+        <li>
+            <div className={styles.des}>
+                {description} 
+            </div>
+            <div className={styles.deleteButton}>
+                <Button variant='contained' color='secondary'>Delete</Button>
+            </div>
+        </li>
     )
 }
 
